6032fe8cef5045bbba98eccadf69c6c2fb4dd7df
Pallavi-2007-K <2400032466@kluniversity.in>
2026-04-09 22:18:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import WebinarListPage from "./pages/WebinarListPage";
+import MyRegistrationsPage from "./pages/MyRegistrationsPage";
+import AdminDashboard from "./pages/AdminDashboard";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<WebinarListPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/my-registrations" element={<MyRegistrationsPage />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
